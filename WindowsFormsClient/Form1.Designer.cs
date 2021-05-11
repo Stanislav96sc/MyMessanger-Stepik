bed41e381fd86b6a919067aeec595d948a92a4a7
Stanislav96sc <stasik277@gmail.com>
2021-05-11 20:42:37 +0300

    Дабавлен логика для работы кнопки "Отправить"

---
+++ WindowsFormsClient/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace WindowsFormsClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.UserNameTB = new System.Windows.Forms.TextBox();
+            this.MessageTB = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(265, 179);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(75, 46);
+            this.SendButton.TabIndex = 0;
+            this.SendButton.Text = "Отправить";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(328, 160);
+            this.listBox1.TabIndex = 1;
+            // 
+            // UserNameTB
+            // 
+            this.UserNameTB.Location = new System.Drawing.Point(13, 179);
+            this.UserNameTB.Name = "UserNameTB";
+            this.UserNameTB.Size = new System.Drawing.Size(243, 20);
+            this.UserNameTB.TabIndex = 2;
+            // 
+            // MessageTB
+            // 
+            this.MessageTB.Location = new System.Drawing.Point(13, 205);
+            this.MessageTB.Name = "MessageTB";
+            this.MessageTB.Size = new System.Drawing.Size(243, 20);
+            this.MessageTB.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 238);
+            this.Controls.Add(this.MessageTB);
+            this.Controls.Add(this.UserNameTB);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.SendButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox UserNameTB;
+        private System.Windows.Forms.TextBox MessageTB;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
